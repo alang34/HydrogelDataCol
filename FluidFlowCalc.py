24e8d808af6e445e7586fac71c7de2fb72dc0f20
alang34 <alan.gao05@gmaol.com>
2025-07-31 16:05:28 -0400

    code imp of sheet

---
+++ FluidFlowCalc.py
@@ -0,0 +1,39 @@
+import matplotlib as plt
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+# System Constants
+# in square meters, m/s, and designated
+tubeCrossSection = 0.0000712423
+snakeCrossSection = 0.00064516
+snakeLength = 2.87
+snakeHydraulicDiameter = 0.0254 
+gasConstant = 287 # J/(kg*K)
+airViscosity = 1.846e-5 # Pa*s
+gelTemp = 300
+
+#input conditions
+#Singular input
+
+inletTemp = 300
+inletPressure = 101325 # Pa
+inletDensity = inletPressure / (gasConstant * inletTemp) # kg/m^3
+volumetricFlowRateSCFH = 30 # scfh
+
+volumetricFlowRate = volumetricFlowRateSCFH * 0.0000078658
+snakeVelocity = volumetricFlowRate / snakeCrossSection
+snakeReynolds = (inletDensity*snakeVelocity*snakeHydraulicDiameter) / airViscosity
+timeInSnake = snakeLength / snakeVelocity
+
+print(snakeVelocity)
+# df = pd.read_csv('data1.csv')
+# a = df.to_numpy()
+# timestampl = a[0:, 0]
+# inTemp = a[0:, 1]
+# inAbsHum = a[0:, 2]
+# inRelHum = a[0:, 3]
+# outTemp = a[0:, 4]
+# outAbsHum = a[0:, 5]
+# outRelHum = a[0:, 6]
+
