4ecc112d924b515929e74c7c33b255803cf9ed77
Alan Gao <51537789+alang34@users.noreply.github.com>
2025-09-02 09:11:53 -0400

    Create NonFlowTimestamper.py

---
+++ NonFlowTimestamper.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_2.csv")
+
+df.insert(0, 'Timestamp', [i * 2 for i in range(len(df))])
+
+df.to_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_2.csv", index=False)
+
+plt.figure(figsize=(8, 6))
+plt.plot(df["RH"], df["Temp"], marker='o')
+plt.xlabel("RH")
+plt.ylabel("Temp")
+plt.title(f'RH vs Temp')
+plt.grid(True)
+plt.show()
