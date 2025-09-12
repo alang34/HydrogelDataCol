8bf2ad393536a3181dcc4af04a6dcfb9fb1134de
alang34 <alan.gao05@gmail.com>
2025-09-12 18:29:28 -0400

    nonflow run 3

--- NonFlowGrapher.py
+++ NonFlowGrapher.py
@@ -2,13 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_2.csv")
+df = pd.read_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_3.csv")
 
 
 plt.figure(figsize=(8, 6))
-plt.plot(df["AH"], df["Temp"])
-plt.xlabel("Absolute Humidity")
-plt.ylabel("Temperature (°C)")
-plt.title(f'AH vs Temp - Run 2')
+plt.plot(df["RH"], df["Temp"])
+plt.xlabel("Timestamp")
+plt.ylabel("Relative Humidity (%)")
+plt.title(f'RH vs Timestamp - Run 3')
 plt.grid(True)
 plt.show()
--- NonFlowTimestamper.py
+++ NonFlowTimestamper.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_1.csv")
+df = pd.read_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_3.csv")
 
 df.insert(0, 'Timestamp', [i * 2 for i in range(len(df))])
 
-df.to_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_1.csv", index=False)
+df.to_csv("HydrogelDataCol/Actual Data/Nonflow Data/nonflow_run_3.csv", index=False)
 
 plt.figure(figsize=(8, 6))
 plt.plot(df["RH"], df["Temp"], marker='o')
